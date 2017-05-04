c21f27098fa7f92c63e0006a287fe138229a79ba
venilavino <venilavino100@gmail.com>
2017-05-04 17:37:50 +0530

    Create odd_20.py

---
+++ odd_20.py
@@ -0,0 +1,22 @@
+
+start = 0
+end = 20
+sum = 0
+n = 0
+ 
+print "Enter Starting Number:", 
+start = input()
+print "Enter Ending Number:", 
+end = input()
+ 
+n = start
+if ( (n  % 2) == 0):
+   n += 1
+ 
+print "Adding Numbers : ", 
+while(n <= end):
+   print n, 
+   sum += n
+   n += 2
+ 
+print "\nThe result is: ", sum
