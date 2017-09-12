97e4c9c23f6a2a14ea9a9b45798df6d3421b03f4
venilavino <venilavino100@gmail.com>
2017-09-12 17:12:01 +0530

    Update string_contains.java

--- string_contains.java
+++ string_contains.java
@@ -3,8 +3,7 @@ class Example {
 public static void main(String[] args)  
     {  
         String str1 = "hey !!! Hello ";  
-        String str2 = "Hello";  
-System.out.println();  
+        String str2 = "Hello";   
 System.out.println("String 1: " + str1);  
 System.out.println("String 2: " + str2);  
 System.out.println(str1.contains(str2));  
