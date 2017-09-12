8d7b23130756a2850068cf0877f07881dd406f4e
venilavino <venilavino100@gmail.com>
2017-09-12 17:10:02 +0530

    Create string_contains.java

---
+++ string_contains.java
@@ -0,0 +1,13 @@
+class Example {  
+  
+public static void main(String[] args)  
+    {  
+        String str1 = "hey !!! Hello ";  
+        String str2 = "Hello";  
+System.out.println();  
+System.out.println("String 1: " + str1);  
+System.out.println("String 2: " + str2);  
+System.out.println(str1.contains(str2));  
+System.out.println();  
+    }  
+}  
